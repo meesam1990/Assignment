96c2477d453093b6f4739d7a1a4b0f68d35581ce
Mohd Meesam <mohdmeesam@gmail.com>
2018-04-27 00:33:34 +0530

    updating config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAI5CGUKTQTPHTYYMA"
-  secret_key = "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"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
 }
